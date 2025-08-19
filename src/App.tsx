cc4ee34ab177e883128bd1cb6a4bfdec59456a85
mykhailo <m.diakovych13@gmail.com>
2025-08-19 13:12:35 +0300

    chore: remove unused assets and CSS in favor of SCSS styling with a new variables file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import { useState } from "react";
 
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
    const [count, setCount] = useState(0);
 
    return (
-      <>
-         <div>
-            <a href="https://vite.dev" target="_blank" rel="noreferrer">
-               <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-
-            <a href="https://react.dev" target="_blank" rel="noreferrer">
-               <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-         </div>
+      <main>
          <h1>Vite + React</h1>
          <div className="card">
             <button onClick={() => setCount((count) => count + 1)}>
@@ -31,7 +18,7 @@ function App() {
          <p className="read-the-docs">
             Click on the Vite and React logos to learn more
          </p>
-      </>
+      </main>
    );
 }
 
